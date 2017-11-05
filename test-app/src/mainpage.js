10ea34c6e7fee0a009e9d48ab89a8f6f22ab4919
Gabriel Angel Amarista Rodrigues <gaborod16@gmail.com>
2017-11-05 20:22:02 +0000

    Fundo a branco, botao (+) acrescentado

--- test-app/src/mainpage.js
+++ test-app/src/mainpage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button, Well, FormControl, FormGroup, ControlLabel, Row, Col} from 'react-bootstrap';
+import {Button, Well, FormControl, FormGroup, ControlLabel, Row, Col, Image} from 'react-bootstrap';
 
 import Template from './template';
 
@@ -25,93 +25,24 @@ class MainPage extends Component {
    
 
   render() {
-      
-      var container_username={
-          textAlign: "center",
-          paddingTop: "0.1px"
-      }
-      
-      var username = {
-          fontSize: "25px",
-          fontWeight: "normal",
-          color: "white",
-          opacity: "0.9"
-      }
-      
-      var botao_new_bsp = {
-          backgroundColor: "red",
-          width: "70px",
-          height: "70px",
-          marginTop: "-60%",
-          marginLeft: "10%",
-          borderRadius: "100px",
-          border: "1px solid white",
-          backgroundColor: "#c3c3c9",
-          textAlign: "center"
-          
-      }
-      
-      var plus ={
-          fontSize: "35px",
-          fontWeight: "lighter",
-          color: "white",
-          opacity: "0.9",
-          marginTop: "10px"
-      }
-      
-       var container_bsps ={
-          
-          height: "200px",
-          width: "40%",
-          border: "2px solid white",
-          borderStyle: "dotted", 
-          marginLeft: "32%",
-          marginTop: "-9%",
-          borderRadius: "15px",
-           textAlign: "center"
-     }
-       
-       var info_bsp={
-          fontSize: "25px",
-          fontWeight: "lighter",
-          color: "white",
-          opacity: "0.9",
-           marginTop: "-13%",
-           
-       }
-       
-       var lista_bsps={
-             borderRadius: "15px",
-           textAlign: "center",
-           backgroundColor: "#f8c97f",
-           width: "80%",
-           height: "20%",
-          
-           margin: "auto",
-           textAlign: "center"
-       }
-      
-       var project_text = {
-             fontSize: "25px",
-          fontWeight: "bold",
-          color: "white",
-          opacity: "0.9",
-          
-       }
-       
     return (
       
       <Template>
         <Row> 
-          <Col md={3} sm={4} onClick={this.redirectNewProject}>
-            <h2> + <small>Create New Project</small></h2>
+          <Col md={3} sm={4}>
+            <Col>
+              <Image src="assets/plus.png" circle className="small-image" onClick={this.redirectNewProject}/>
+            </Col>
+            <Col>
+              <h5>Create New Project</h5>
+            </Col>
           </Col>
 
           <Col lg={6} md={7} sm={8}>
             <h2 className="center-text"> Your BSP Projects</h2>
             
-            <Button bsStyle='warning' bsSize='medium' onClick={this.redirectProjectPage} block><h4>Project #1</h4></Button>
-            <Button bsStyle='warning' bsSize='medium' onClick={this.redirectProjectPage} block><h4>Project #2</h4></Button>
+            <Button bsStyle='warning' onClick={this.redirectProjectPage} block><h4>Project #1</h4></Button>
+            <Button bsStyle='warning' onClick={this.redirectProjectPage} block><h4>Project #2</h4></Button>
             
           </Col>
         </Row>
